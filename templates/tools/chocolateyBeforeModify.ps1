bb49f15d92c9cd783cb3f343fd6f305efc34ca0b
Micah Hunsberger <mhuns14@gmail.com>
2020-10-01 12:13:57 -0400

    nssm needs to exit before upgrade/uninstall

--- templates/tools/chocolateyBeforeModify.ps1
+++ templates/tools/chocolateyBeforeModify.ps1
@@ -1,7 +1,8 @@
+$ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ScriptName = 'startservice'
 
-$ServiceConfig = Import-PowerShellDataFile -Path "$toolsDir\$ScriptName.psd1"
+$ServiceConfig = Import-PowerShellDataFile -Path "$toolsDir\$ScriptName.psd1" -ErrorAction SilentlyContinue
 if ([string]::IsNullOrWhiteSpace($ServiceConfig.Name)) { $ServiceConfig.Name = $env:ChocolateyPackageName }
 if ([string]::IsNullOrWhiteSpace($ServiceConfig.DisplayName)) { $ServiceConfig.DisplayName = $env:ChocolateyPackageTitle }
 if (!$ServiceConfig.ShutdownTimeout) { $ServiceConfig.ShutdownTimeout = 5000 }
@@ -12,4 +13,5 @@ if ($service) {
     $service | Stop-Service -NoWait -ErrorAction Continue
     $timeout = New-TimeSpan -Seconds (($ServiceConfig.ShutdownTimeout/1000) + 10)
     $service.WaitForStatus('Stopped', $timeout)
+    Start-Sleep -Seconds 2 # give time for nssm to actually exit. Otherwise Chocolatey will try to remove the working directory while nssm is still running
 }
